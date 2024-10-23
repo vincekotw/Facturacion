7e9eac2e8c0cf657245193e5aacf2f9660e90a5f
vincekotw <156933306+vincekotw@users.noreply.github.com>
2024-10-23 19:33:35 -0400

    prueba settings.py

--- Factura/settings.py
+++ Factura/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
